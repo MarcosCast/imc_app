6d443e69816490569e88cc304cf9a7eee03bad85
MarcosCast <marcosmax.5@gmail.com>
2022-12-31 08:02:02 -0300

    Criando os inputs

--- src/components/Imc.jsx
+++ src/components/Imc.jsx
@@ -4,6 +4,18 @@ function IMC() {
     return(
         <div className="massa">
             <h1>Indice de Massa Corporal</h1>
+            <div className="inputs">
+                <div className="altura">
+                    "Altura"
+                    <span>ex.: 1,70</span>
+                    <input type="text" className="input_altura" placeholder="Metros"/>
+                </div>
+                <div className="peso">
+                    "Quilos"
+                    <span>ex.: 75,2</span>
+                    <input type="text" className="input_peso" placeholder="Quilos"/>
+                </div>
+            </div>
         </div>
     );
 }
