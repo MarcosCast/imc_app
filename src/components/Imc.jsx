0c18037c5ac68c08ccb6e2ba19d2d71aa9d22a22
MarcosCast <marcosmax.5@gmail.com>
2022-12-31 08:12:05 -0300

    Estilizando com scss

--- src/components/Imc.jsx
+++ src/components/Imc.jsx
@@ -6,13 +6,16 @@ function IMC() {
             <h1>Indice de Massa Corporal</h1>
             <div className="inputs">
                 <div className="altura">
-                    "Altura"
-                    <span>ex.: 1,70</span>
+                     Altura
+                    <span> ex.: 1,70 </span>
+                    <br />
                     <input type="text" className="input_altura" placeholder="Metros"/>
                 </div>
+                <br />
                 <div className="peso">
-                    "Quilos"
-                    <span>ex.: 75,2</span>
+                     Quilos
+                    <span> ex.: 75,2 </span>
+                    <br />
                     <input type="text" className="input_peso" placeholder="Quilos"/>
                 </div>
             </div>
