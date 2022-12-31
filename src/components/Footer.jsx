146591bbed63d2ff8489a5270e31e15bf47a7e76
MarcosCast <marcosmax.5@gmail.com>
2022-12-31 08:20:24 -0300

    Adicionando o Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+import Github from '../images/github.png';
+import Linkedin from '../images/linkedin.png';
+
+function Rodape() {
+    return (
+        <footer class="rodape">
+        <p>Calculadora IMC em React</p>
+        <span>Desenvolvido por Marcos Castro</span>
+        <div class="rodape-media">
+          <a href="https://github.com/MarcosCast"><img src={Linkedin} alt="github"/></a>
+        
+          <a href="https://www.linkedin.com/in/marcos--castro/"><img src={Github} alt="linkedin"/></a>
+        </div>
+        </footer>        
+    )    
+}
+
+export default Rodape;
