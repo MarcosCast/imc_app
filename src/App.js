15108e5999192f56961c6820c6829b71883ed3d2
MarcosCast <marcosmax.5@gmail.com>
2022-12-31 08:23:38 -0300

    Verificar pq a import do Footer esta dando erro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import IMC from './components/Imc';
+//import Rodape from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <IMC />
-    
+    //<Rodape />    
   );
 }
 
